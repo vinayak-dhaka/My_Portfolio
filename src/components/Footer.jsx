fe38d711973fd0498fc25a8cb2971edc0350af0a
vinayak dhaka <vinayakdhaka94@gmail.com>
2025-11-06 21:03:12 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <footer>
-      <p>© 2025 Vinayak Dhaka. All rights reserved.</p>
+      <p> © 2025 Vinayak Dhaka. All rights reserved. Use for inspiration . Don't claim as your own.</p>
     </footer>
   );
 };
